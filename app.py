04fcbbfbbd20479768e6c61abe085f1f7bb236df
KibuuleNoah <noahkibuule3@gmail.com>
2023-10-28 23:49:27 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    return render_template("index.html")
+
+
+@app.route("/get-questions")
+def get_questions():
+    with open("./questions.json", "r") as file:
+        file = file.read()
+        return jsonify(file)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,92 @@
+let checkAnswerBtn = document.getElementById("check-answer");
+let nextBtn = document.getElementById("nextBtn"); 
+
+let n = 0;
+let correct = 0;
+let wrong = 0;
+let current_qtn = 0;
+let questionIdx = 0
+var test;
+
+//disable Check Anwer button before picking any answer
+checkAnswerBtn.disabled = true;
+
+// switches from the finished question to a new one
+const change_board = (test)=>{
+  let board = document.getElementById("board");
+  let new_boad = document.createElement("div")
+  new_boad.innerHTML = `
+  <div id="question">${test[questionIdx].question}</div>
+  <br>
+  <input type="radio" id="option1" name="opt" value=${test[questionIdx].optn1}>
+  <label for="option1">${test[questionIdx].optn1}</label>
+  <br>
+  <input type="radio" id="option2" name="opt" value=${test[questionIdx].optn2}>
+  <label for="option2">${test[questionIdx].optn2}</label>
+  <br>
+  <input type="radio" id="option3" name="opt" value=${test[questionIdx].optn3}>
+  <label for="option3">${test[questionIdx].optn3}</label>
+  <br>
+  <input type="radio" id="option4" name="opt" value=${test[questionIdx].optn4}>
+  <label for="option4">${test[questionIdx].optn4}</label>
+  `;
+  new_boad.id = "board";
+  document.body.replaceChild(new_boad,board);
+  document.getElementById("out-of").innerText = `${questionIdx+1}/${test.length} questions`
+}
+
+// Checks if the selected answer is correct or not
+const checkIsCorrect = (answer) => {
+  let selectedValue =document.querySelector('input[name="opt"]:checked').value;
+  if (selectedValue === answer){
+    document.querySelector(`input[value=${answer}]`).nextSibling.nextSibling.style.backgroundColor = "#59f179"
+    correct++;
+    update_correct_wrong();
+  }
+  else{
+    document.querySelector(`input[value=${selectedValue}]`).nextSibling.nextSibling.style.backgroundColor = "#f13959"
+    document.querySelector(`input[value=${answer}]`).nextSibling.nextSibling.style.backgroundColor = "#59f179"
+    wrong++;
+    update_correct_wrong();
+  }
+}
+
+// updates the number of both correct and wrong answers
+const update_correct_wrong = ()=>{
+  document.getElementById("correct").innerText = `Correct: ${correct}`;
+  document.getElementById("wrong").innerText = `Wrong: ${wrong}`;
+}
+
+// listens on all the given  options (answers)
+// if any of them is selected it activates the "Check Answer" button
+document.body.addEventListener("input",()=>{
+  checkAnswerBtn.disabled = false;
+})
+
+//fetch question from the backend
+fetch('/get-questions')
+  .then(response => response.json())
+  .then(data => {
+
+    //convert json to a javascript object
+    test = JSON.parse(data);
+
+    // it runs the "change_board" function so that the first question is displayed 
+    change_board(test);
+
+    // listens on the "Check Answer" button 
+    // if it's or not clicked inorder to submit to answer
+    checkAnswerBtn.addEventListener("click",() =>{
+      nextBtn.style.display = "inline-block";
+      checkAnswerBtn.replaceWith(nextBtn)
+      checkIsCorrect(test[questionIdx].answer);
+      questionIdx++;
+    })
+
+    // listens on the "Next" button inorder to switch to a new question
+    nextBtn.addEventListener("click",() =>{
+      change_board(test)
+      checkAnswerBtn.disabled = true;
+      nextBtn.replaceWith(checkAnswerBtn);
+    })
+})
